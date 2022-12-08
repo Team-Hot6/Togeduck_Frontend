9b85ae500e160a0f6c1157b3a145c1b21dcc50c7
jihyun-cho-0 <roseview808@gmail.com>
2022-12-08 20:27:11 +0900

    create : 취미카테고리 클릭 시 해당 워크샵 출력
    - 메인 화면에서 특정 카테고리 클릭 시 해당하는 워크샵 목록만 출력
    - 만약 해당하는 워크샵이 존재하지 않는다면 에러 메시지를 화면에 출력

--- js/main/workshop.js
+++ js/main/workshop.js
@@ -1,5 +1,6 @@
 console.log("workshop.js 연결완료");
 workshop_list()
+hobby_list()
 
 // 모든 워크샵 목록 출력
 async function workshop_list(){
@@ -8,14 +9,22 @@ async function workshop_list(){
     if(response.status == 200){
         data = await response.json()
 
+        gridbox = document.getElementById("gridbox")
+        gridbox.innerHTML = ''
+
         for (i = 0; i < data.length; i++) {
-            console.log(`${[i]}번 워크샵 pk : ${data[i]['pk']}`) 
-            console.log(`${[i]}번 워크샵 취미카테고리 : ${data[i]['category']}`) 
-            console.log(`${[i]}번 워크샵 지역 : ${data[i]['location']}`) 
-            console.log(`${[i]}번 워크샵 제목 : ${data[i]['title']}`)
-            console.log(`${[i]}번 워크샵 소개글 : ${data[i]['content']}`) 
-            console.log(`${[i]}번 워크샵 이미지 : ${data[i]['workshop_image']}`)
-            console.log("-------------")
+         
+            const workshop = `<div class="workshop" style="background-color: #ffffff;">
+                                <div class="workshop_img_frame">
+                                    <img class="workshop_img" src="${back_end_url}${data[i]['workshop_image']}">
+                                </div>
+                                <span class="date">${data[i]['date']} | ${data[i]['cur_time']}</span>
+                                <div class="title">${data[i]['title']}</div>
+                                <span class="hobby_category">${data[i]['category']}</span>
+                                <span class="location">${data[i]['location']}</span>
+                            </div>`;
+            
+            gridbox.insertAdjacentHTML("beforeend", workshop);
         }
     }
 }
@@ -23,8 +32,54 @@ async function workshop_list(){
 // 모든 취미 카테고리 목록 출력
 async function hobby_list(){
     const response = await hobby_get()
+   
+    if(response.status == 200){
+        data = await response.json()
+ 
+        hobbys = document.getElementById("hobbys")
+        hobbys.innerHTML = ''
+
+        for (i = 0; i < data.length; i++) {
+       
+            const hobby = `<button type="button" class="hobby" onclick="workshop_pick_list(${data[i]['id']})">${data[i]['category']}</button>`;
+
+            hobbys.insertAdjacentHTML("beforeend", hobby);
+        }
+    }
+}
+
+// 특정 카테고리 선택 시 해당하는 워크샵 목록 출력
+async function workshop_pick_list(category_id){
+    
+    const response = await workshop_pick_get(category_id)
+    console.log(response)
 
     if(response.status == 200){
         data = await response.json()
+
+        gridbox = document.getElementById("gridbox")
+
+        if(data.length == 0){
+            gridbox.innerHTML = `<div>선택하신 카테고리의 워크샵이 없습니다.</div>`
+           
+        }
+        else{
+            gridbox.innerHTML = ''
+
+            for (i = 0; i < data.length; i++) {
+            
+                const workshop = `<div class="workshop" style="background-color: #ffffff;">
+                                    <div class="workshop_img_frame">
+                                        <img class="workshop_img" src="${back_end_url}${data[i]['workshop_image']}">
+                                    </div>
+                                    <span class="date">${data[i]['date']} | ${data[i]['cur_time']}</span>
+                                    <div class="title">${data[i]['title']}</div>
+                                    <span class="hobby_category">${data[i]['category']}</span>
+                                    <span class="location">${data[i]['location']}</span>
+                                </div>`;
+                
+                gridbox.insertAdjacentHTML("beforeend", workshop);
+            }
+        }
     }
 }
--- js/main/workshop_api.js
+++ js/main/workshop_api.js
@@ -11,7 +11,15 @@ async function workshop_get() {
 
 // 모든 취미 카테고리 목록 출력
 async function hobby_get() {
-    const response = await fetch(`${back_end_url}/hobby/`, {
+    const response = await fetch(`${back_end_url}/workshops/hobby/`, {
+        method:'GET'
+    })
+    return response
+}
+
+// 특정 카테고리 선택 시 해당하는 워크샵 목록 출력
+async function workshop_pick_get(category_id) {
+    const response = await fetch(`${back_end_url}/workshops/?category=${category_id}`, {
         method:'GET'
     })
     return response
