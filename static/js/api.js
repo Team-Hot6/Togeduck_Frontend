01203f0c8e3d68dbdde5ab5afcf2049d439eb2f8
9yuhyeon <kkh2742@gmail.com>
2022-12-09 21:47:36 +0900

    create : 커뮤니티 페이지 카테고리 선택 시 해당 카테고리의 게시글만 조회하는 기능 추가

--- static/js/api.js
+++ static/js/api.js
@@ -7,7 +7,15 @@ const frontend_base_url = "http://127.0.0.1:5000"
 // 로그인 된 사용자 정보 
 const token = localStorage.getItem("access");
 
-// 게시글 전체보기 api 호출
+// 카테고리 목록 API
+async function get_hobby() {
+    const response = await fetch(`${backend_base_url}/workshops/hobby/`, {
+        method: "GET",
+    })
+    return response
+}
+
+// 게시글 전체 목록 API
 async function get_articles() {
     const response = await fetch(`${backend_base_url}/articles/`, {
         method: "GET",
@@ -15,7 +23,7 @@ async function get_articles() {
     return response
 }
 
-// 게시글 작성하기 버튼
+// 게시글 작성하기 버튼 API
 function handleArticleCreate(){
     console.log('게시글 작성 버튼')
     if (token){
@@ -24,4 +32,12 @@ function handleArticleCreate(){
     } else {
         alert('게시글 작성은 로그인 된 사용자만 가능합니다!')
     }
+}
+
+// 카테고리 선택 시 해당 카테고리 게시글 리스트 API
+async function get_select_articles(category_id) {
+    const response = await fetch(`${backend_base_url}/articles/?category=${category_id}`, {
+        method: "GET",
+    })
+    return response
 }
