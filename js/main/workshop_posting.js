52fc95b42eab25e6b3d17b2f1e1e2a52cae5cce7
jihyun-cho-0 <roseview808@gmail.com>
2022-12-13 20:11:56 +0900

    update : 워크샵 생성

--- js/main/workshop_posting.js
+++ js/main/workshop_posting.js
@@ -43,3 +43,35 @@ async function location_category_list(){
         }
     }
 }
+
+
+$("document").ready(function () {
+
+    var textmax=500;
+   
+    $("#count").text(textmax + ' character left');
+    
+    $("#bio").keyup(function(){
+        
+        var userlenght= $("#bio").val().length;
+        
+        var remain= textmax - userlenght ;
+  
+        $("#count").text(remain + ' characters left');
+        
+    });
+    
+  });
+  
+  document.getElementById('getval').addEventListener('change', readURL, true);
+  function readURL(){
+    var file = document.getElementById("getval").files[0];
+    var reader = new FileReader();
+    reader.onloadend = function(){
+        document.getElementById('profile-upload').style.backgroundImage = "url(" + reader.result + ")";        
+    }
+    if(file){
+        reader.readAsDataURL(file);
+    }else{
+    }
+  }
